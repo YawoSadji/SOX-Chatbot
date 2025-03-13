339d032e464731dc3535f60970818adda8e0cc40
YawoSadji <yawo.sadji@gmx.com>
2025-03-13 11:20:52 -0400

    added request and response from api in ChatForm

--- src/components/ChatForm.jsx
+++ src/components/ChatForm.jsx
@@ -1,18 +1,55 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+const genAI = new GoogleGenerativeAI(import.meta.env.API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
 export default function ChatForm() {
+  const [userMessage, setUserMessage] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const handleUserMessage = (e){
+    e.preventDefault();
+    sendMessage();
+  }
+  const sendMessage = async () => {
+    if(userMessage.trim() === "") return;
+    setIsLoading(true);
+    try {
+        const result = await model.generateContent(userMessage);
+        if(!result || !result.response){
+            throw new Error("Invalid API response");
+        };
+        const botResponse = result.response.text();
+        setChatHistory((prevHistory) => [
+            ...prevHistory,
+            { type: "user", message: userMessage }, { type: "bot", message: botResponse }
+        ]);
+    } catch (error) {
+        console.error(error);
+    }
+    finally{
+        setUserMessage("");
+        setIsLoading(false);
+    }
+  }
+
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Control
-          className="mr-sm-2"
-          type="text"
-          placeholder="Ask me about SOX..."
-        />
-      </Form.Group>
-      <Button variant="primary">Primary</Button>
+    <Form onSubmit={handleUserMessage}>
+      <Row className="align-items-center">
+        <Col xs="auto">
+          <Form.Control
+            className="mr-sm-2"
+            type="text"
+            placeholder="Ask me about SOX..."
+          />
+        </Col>
+        <Col xs="auto">
+          <Button variant="primary">Primary</Button>
+        </Col>
+      </Row>
     </Form>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 export default function Header() {
   return (
@@ -11,8 +10,8 @@ export default function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">History</Nav.Link>
+            <Nav.Link href="#link">Logout</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
